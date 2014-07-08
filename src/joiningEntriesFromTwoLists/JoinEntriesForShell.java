9a7100f13c0321aef6b9c13b7b1b36f86d6c1ce6
Fernando Carrillo <fernando@carrillo.at>
2014-07-08 11:02:22 -0400

    Added support for joining all fields of lists without specifying fields of interest.

--- src/joiningEntriesFromTwoLists/JoinEntriesForShell.java
+++ src/joiningEntriesFromTwoLists/JoinEntriesForShell.java
@@ -37,8 +37,35 @@ public class JoinEntriesForShell {
 		getInputList1().readList( false ); 
 		getInputList2().readList( false ); 
 		
+		if( getInputFile1FieldsOfInterest() == null )
+		{
+			this.inputFile1FieldsOfInterest = getAllFieldsWithoutId( getInputList1() ); 
+		}
+		if( getInputFile2FieldsOfInterest() == null )
+		{
+			this.inputFile2FieldsOfInterest = getAllFieldsWithoutId( getInputList2() ); 
+		}
+		
 		//System.out.println( getInputList1() ); 
 		//System.out.println( getInputList2() ); 
+	}
+	
+	private int[] getAllFieldsWithoutId( final List list )
+	{
+		final String[] entry = list.getEntriesArrayList().get( 0 ); 
+		final int[] fieldIndices = new int[ entry.length - 1 ]; 
+		
+		int j = 0; 
+		for( int i = 0; i < entry.length; i++ )
+		{
+			if( i != list.getIdField() )
+			{
+				fieldIndices[ j ] = i; 
+				j++; 
+			}
+		}
+		
+		return fieldIndices; 
 	}
 	
 	public void generateOutputFile() {
@@ -62,18 +89,21 @@ public class JoinEntriesForShell {
 	
 	public static void main(String[] args) 
 	{
-		if( args.length != 4 )
+		if( args.length != 4 && args.length != 2 )
 		{
 			final String info = "\n#######################\n" + 
 					"java -jar joinListEntries inputList1 -fieldsToKeepinList1=[int,int,int...] inputList2 -fieldsToKeepinList2=[int,int,int...] \n" +
-					"#######################\n" + 
+					"#######################\n" +
+					"alternatively specify only the lists and all entries will be kept:\n" + 
+					"java -jar joinListEntries inputList1 inputList2\n" +
+					"#######################\n" +
 					"Joins two list if rows share a common id in column 1.\n" +
 					"Specify the tab separated list1 (inputList1) and the columns to keep by providing an comma-separated array of integer values -fieldsToKeepinList1=[int,int,int...].\n" +
 					"Specify the tab separated list2 (inputList2) and the columns to keep by providing an comma-separated array of integer values -fieldsToKeepinList2=[int,int,int...].\n" +
-					"\n\n"; 
-			System.err.println( info ); 
+					"\n"; 
+			System.err.println( info );  
 		}
-		else
+		else if( args.length == 4 )
 		{
  
 			final String inputFile1 = args[ 0 ];
@@ -93,13 +123,23 @@ public class JoinEntriesForShell {
 				for( int i = 0; i < if2.length; i++ )
 					inputFile2FieldsOfInterest[ i ] = Integer.parseInt( if2[ i ] ) - 1 ;  
 			}
-			catch (Exception e) {
-				System.err.println( "Please provide a comma separated list of columns you want to keep in the output dataset!"); 
-				System.exit( 1 ); 
+			catch (Exception e) 
+			{
+				System.err.println( "Cannot read the field of interest parameters: " + e ); 
+				System.exit( 1 );
 			}
 				
 			final String outputFile = "";			
 			new JoinEntriesForShell( inputFile1, inputfile2, outputFile, inputFile1FieldsOfInterest, inputFile2FieldsOfInterest );  
+		}
+		else 
+		{
+			final String inputFile1 = args[ 0 ];
+			final String inputFile2 = args[ 1 ];
+			int[] inputFile1FieldsOfInterest = null;
+			int[] inputFile2FieldsOfInterest = null;
+			final String outputFile = "";			
+			new JoinEntriesForShell( inputFile1, inputFile2, outputFile, inputFile1FieldsOfInterest, inputFile2FieldsOfInterest );
 		}
 	}
 
